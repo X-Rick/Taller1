03c8918bf3ab7507ae7683c4e02ed8835b819827
Ricardo <rick0709xe@gmail.com>
2024-10-18 11:54:48 -0500

    upd/ se mejora la parte de coleccion de capitulos, ahora solo identa en varios items del diccionario, y se añade funciones al boton de siguiente

--- cap1.js
+++ cap1.js
@@ -11,3 +11,43 @@ const EpVideo = document.getElementById('epi');
     window.location.href = 'anime.html'; 
 }); 
 
+
+
+const capitulos = [
+    {
+        id: 1,
+        titulo: 'cap1',
+        url: 'https://terabox.com/sharing/embed?surl=2ZC_YcvIUnGp3yI6H0GMnw&resolution=1080&autoplay=true&mute=false&uk=4399467314851&fid=342618491994056&slid=',
+
+
+    },
+    {
+        id:2,
+        titulo: 'cap2',
+        url: 'https://terabox.com/sharing/embed?surl=G650AuCLkyrkNc-FjicC1g&resolution=1080&autoplay=true&mute=false&uk=4399467314851&fid=728899725397728&slid=" '
+    }
+]
+
+let epid = 1
+
+
+function reproduccirVideo() {
+    const repro = capitulos.find(c => c.id === epid);
+    if (repro) {
+        const ContRepro = document.getElementById('reprod');
+        ContRepro.innerHTML = `<iframe src="${repro.url}" frameborder="1" allowfullscreen class="video1"></iframe>`;
+
+    } else {
+        alert("no hay mas episodios");
+    } 
+}
+
+document.getElementById('sig').addEventListener('click', () => {
+    epid++;
+    if (epid > capitulos.length) {
+        window.location.href = 'anime.html'; //esto se hace para volver en caso de que se llegue al final y quiera avanzar 
+    }
+    reproduccirVideo();
+})
+
+reproduccirVideo();
