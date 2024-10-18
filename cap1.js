1c60df3fd80aa8dfd64e4b6f83a778b083419884
Ricardo <rick0709xe@gmail.com>
2024-10-18 13:02:29 -0500

    add/ahora se habilita que cuando un usuario desee ingresar desde otro capitulo que no sea el primero, el reproductor responda

--- cap1.js
+++ cap1.js
@@ -16,25 +16,32 @@ const EpVideo = document.getElementById('epi');
 const capitulos = [
     {
         id: 1,
-        titulo: 'cap1',
+        titulo: 'Yo diría que así empieza el amor. ¿no crees?',
         url: 'https://terabox.com/sharing/embed?surl=2ZC_YcvIUnGp3yI6H0GMnw&resolution=1080&autoplay=true&mute=false&uk=4399467314851&fid=342618491994056&slid=',
 
 
     },
     {
         id:2,
-        titulo: 'cap2',
-        url: 'https://terabox.com/sharing/embed?surl=G650AuCLkyrkNc-FjicC1g&resolution=1080&autoplay=true&mute=false&uk=4399467314851&fid=728899725397728&slid=" '
+        titulo: 'Eso es un extraterreste, ¿no?',
+        url: 'https://terabox.com/sharing/embed?surl=G650AuCLkyrkNc-FjicC1g&resolution=1080&autoplay=true&mute=false&uk=4399467314851&fid=728899725397728&slid='
+    },
+    {
+        id:3,
+        titulo: 'El choque de la abuela contra abuela',
+        url: 'https://terabox.com/sharing/embed?surl=6O25PBHENKfEZPpZH3A4Lw&resolution=1080&autoplay=true&mute=false&uk=4399467314851&fid=199855654280950&slid='
     }
 ]
 
-let epid = 1
+const NumEpi = new URLSearchParams(window.location.search); //mira en que episiodio empieza a reproducirse
+let epid = parseInt(NumEpi.get('capitulo'));
 
 
 function reproduccirVideo() {
     const repro = capitulos.find(c => c.id === epid);
     if (repro) {
         const ContRepro = document.getElementById('reprod');
+        
         ContRepro.innerHTML = `<iframe src="${repro.url}" frameborder="1" allowfullscreen class="video1"></iframe>`;
 
     } else {
